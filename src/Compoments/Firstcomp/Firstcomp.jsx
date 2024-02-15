521e7967d9677a60c12a49785e2508f9a7adefde
Soulayy <s.sultaway@gmail.com>
2024-02-15 14:20:45 +0100

    start the selet choix

--- src/Compoments/Firstcomp/Firstcomp.jsx
+++ src/Compoments/Firstcomp/Firstcomp.jsx
@@ -2,20 +2,22 @@ import "./Firstcomp.css";
 
 export default function Firstcomp(props) {
     return (
-        <div className="h-[35%] w-[100%] flex justify-center items-center">
-            <div className="h-[200px] w-[700px]  rounded-xl border-[2px] flex justify-center items-center">
-                <div className="h-[90%] w-[45%] flex justify-center items-start flex-col">
-                    <p className="text-white text-5xl font-semibold">ROCK</p>
-                    <p className="text-white text-5xl font-semibold">PAPER</p>
-                    <p className="text-white text-5xl font-semibold">SCISSORS</p>
-                </div>
-                <div className="h-[80%] w-[45%] flex justify-center items-end flex-col">
-                    <div className="h-[90%] w-[50%] bg-white text-gray-600 rounded-xl flex justify-center items-center flex-col">
-                        <p className="text-2xl font-semibold">SCORE</p>
-                        <p className="text-6xl font-bold">{props.score}</p>
-                    </div>
-                </div>
-            </div>
+        <div className='fTriangle h-[65%] w-[100%] flex justify-center items-center'>
+        <div className='h-[85%] w-[90%] flex justify-center items-center relative '>
+
+          <div className='h-[140px] w-[140px] rounded-full border-[15px] bg-white border-red-600 absolute bottom-[10%] flex justify-center items-center shadow-gray-600 shadow-inner'>
+            <img onClick={() => props.setChoix("Rock")} src={props.Rock} alt="" srcset="" />
+          </div>
+
+          <div className='h-[140px] w-[140px] rounded-full border-[15px] bg-white border-blue-700 absolute left-[35%] top-[1%] flex justify-center items-center shadow-gray-600 shadow-inner'>
+            <img onClick={() => props.setChoix("Papper")} src={props.Papper} alt="" srcset="" />
+          </div>
+
+          <div className='h-[140px] w-[140px] rounded-full  border-[15px] bg-white border-yellow-300 absolute right-[35%] top-[1%] flex justify-center items-center shadow-gray-600 shadow-inner'>
+            <img onClick={() => props.setChoix("Scissors")} src={props.Scissors} alt="" srcset="" />
+          </div>
+
         </div>
+      </div>
     )
 }
--- src/Compoments/Secondcomp/Secondcomp.jsx
+++ src/Compoments/Secondcomp/Secondcomp.jsx
@@ -3,7 +3,46 @@ import "./Secondcomp.css";
 export default function Secondcomp(props) {
     return (
         <div className="h-[65%] w-[100%] flex justify-center items-center">
-            <div className="h-[100%] w-[90%] flex justify-center items-center bg-blue-600">
+            <div className="h-[100%] w-[90%] flex justify-center items-center ">
+                <div className="h-[80%] w-[40%]  flex justify-around items-center flex-col ">
+                    <div>
+                        <p className="text-white font-bold text-3xl">YOUR PICKED</p>
+                    </div>
+                    <div>
+                        {
+                            props.choix == "Rock" ? 
+                            <div className='h-[200px] w-[200px] rounded-full  border-[15px] bg-white border-red-600 right-[35%] top-[1%] flex justify-center items-center shadow-gray-600 shadow-inner'>
+                            <img src={props.Rock} alt="" />
+                            </div>
+                            :
+                            ""
+                        }
+                        {
+                            props.choix == "Papper" ? 
+                            <div className='h-[200px] w-[200px] rounded-full  border-[15px] bg-white border-blue-700 right-[35%] top-[1%] flex justify-center items-center shadow-gray-600 shadow-inner'>
+                            <img src={props.Papper} alt="" />
+                            </div>
+                            :
+                            ""
+
+                        }
+                        {
+                            props.choix == "Scissors" ? 
+                            <div className='h-[200px] w-[200px] rounded-full  border-[15px] bg-white border-yellow-300 right-[35%] top-[1%] flex justify-center items-center shadow-gray-600 shadow-inner'>
+                            <img src={props.Scissors} alt="" />
+                            </div>
+                            :
+                            ""
+
+                        }
+                    </div>
+                </div>
+                <div className="w-[20%] h-[99%] bg-lime-600 flex justify-center items-center">
+                    <p className="text-5xl">OUI</p>
+                </div>
+                <div className="h-[99%] w-[40%] bg-green-600">
+
+                </div>
                 
             </div>
         </div>
